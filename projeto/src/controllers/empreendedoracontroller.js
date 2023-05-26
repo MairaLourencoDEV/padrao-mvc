896d1e7b8c5fee2c96729eaf078d80137d7d28e4
MairaLourencoDEV <mairalourenco.dev@gmail.com>
2023-05-26 12:12:58 -0300

    Atualização parcial do projeto

---
+++ projeto/src/controllers/empreendedoracontroller.js
@@ -0,0 +1,25 @@
+const empreendedoras = require("../models")
+
+
+
+
+
+
+
+const createEmpreendedoras = (req, res) => {
+    let empreendedora = new empreendedoras(req.body);
+
+    empreendedora.save((err) => {
+        if
+    })
+}
+
+
+
+
+
+
+
+
+
+
